988fcca17ac88d0deab6e70db147a063e78ee386
tigeryu8900 <tigeryu8900@gmail.com>
2024-01-26 18:12:59 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,122 @@
+# reddit-scheduler
+
+A Reddit scheduler for posts
+
+## Set up
+
+Create a `.env` file in the project root directory in this format where `username` and `password` are the Reddit
+credentials.
+
+```dotenv
+USERNAME=username
+PASSWORD=password
+```
+
+Then, create the `~/.reddit/pending/` directory.
+
+Now, for each scheduled post, create a folder of the format `YYYY-mm-dd HH-MM-SS` in `~/.reddit/pending/` where the
+folder name corresponds to the scheduled time.
+
+Create a JSON file at `~/.reddit/pending/YYYY-mm-dd HH-MM-SS/data.json` with these fields:
+
+| fields       | description                                                                                                              |
+|--------------|--------------------------------------------------------------------------------------------------------------------------|
+| `.subreddit` | The name of subreddit with the `r/` part. This could start with `u/` or `user/` instead if we're posting to a user page. |
+| `.title`     | The title of the post.                                                                                                   |
+| `.type`      | The type of post. Check the subsections below for more info                                                              |
+| `.oc`        | Whether or not to mark post as OC. Defaults to `false`.                                                                  |
+| `.spoiler`   | Whether or not to mark post as spoiler. Defaults to `false`.                                                             |
+| `.oc`        | Whether or not to mark post as NSFW. Defaults to `false`.                                                                |
+| `.flair`     | A string representing the flair or `null` for no flair. Defaults to no flair.                                            |
+| `.comments`  | An array of comments to add as strings, or `null` for no comments. Defaults to no comments.                              |
+
+Here's an example `data.json`. `1.png` and `2.png` are in the same directory as `data.json`.
+
+```json
+{
+  "subreddit": "r/subreddit",
+  "title": "title",
+  "type": "gallery",
+  "images": [
+    {
+      "file": "1.png",
+      "caption": "caption",
+      "link": "https://example.com"
+    },
+    {
+      "file": "2.png",
+      "caption": null,
+      "link": null
+    }
+  ],
+  "oc": false,
+  "spoiler": false,
+  "nsfw": false,
+  "flair": "flair",
+  "comments": [
+    "comment 1",
+    "comment 2"
+  ]
+}
+```
+
+### Text posts
+
+For text posts, the `.type` field should be `text` or `post`. Additionally, there are these fields:
+
+| fields  | description                                |
+|---------|--------------------------------------------|
+| `.body` | The body of the post. Defaults to no body. |
+
+### Image posts
+
+For image posts, the `.type` field should be `image`. Additionally, there are these fields:
+
+| fields  | description                                                       |
+|---------|-------------------------------------------------------------------|
+| `.file` | The path to the file of the image post relative to the JSON file. |
+
+### Gallery posts
+
+For gallery posts, the `.type` field should be `gallery` or `images`. Additionally, there are these fields:
+
+| fields               | description                                                              |
+|----------------------|--------------------------------------------------------------------------|
+| `.images`            | An array specifying the file, caption, and link for each image.          |
+| `.images[i].file`    | The path to the file of the image post relative to the JSON file.        |
+| `.images[i].caption` | The caption of the image or `null` for no caption. Defaults to no image. |
+| `.images[i].link`    | The link of the image. Defaults to no image.                             |
+
+### Video posts
+
+For video posts, the `.type` field should be `video`. Additionally, there are these fields:
+
+| fields            | description                                                                                  |
+|-------------------|----------------------------------------------------------------------------------------------|
+| `.file`           | The path to the file of the image post relative to the JSON file.                            |
+| `.thumbnail`      | A number from 1 to 10 inclusive indicating the thumbnail to choose. Defaults to first image. |
+| `.gif`            | Whether or not to convert video to GIF. Defaults to `false`.                                 |
+
+### Video posts
+
+For link posts, the `.type` field should be `link` or `url`. Additionally, there are these fields:
+
+| fields | description           |
+|--------|-----------------------|
+| `.url` | The url for the post. |
+
+## Running
+
+```shell
+# To run in the foreground, use this
+npm run start:fg
+
+# To run in the background, use this
+npm run start:bg
+# or this
+npm start
+```
+
+New folders added to `~/.reddit/pending/` are automatically scheduled.
+
+After the post is made, its folder will be moved to `~/.reddit/done/`.
---
+++ app.js
@@ -0,0 +1,206 @@
+import puppeteer from "puppeteer";
+import "dotenv/config";
+
+import fs from "fs/promises";
+import * as path from "path";
+import os from "os";
+
+const ctxDir = path.join(os.homedir(), ".reddit");
+const userDataDir = path.join(ctxDir, "User Data");
+const pendingDir = path.join(ctxDir, "pending");
+const doneDir = path.join(ctxDir, "done");
+const scheduled = {};
+
+async function post(browser, dir) {
+    console.log(dir, "Starting");
+    const data = JSON.parse((await fs.readFile(path.join(pendingDir, dir, "data.json"))).toString());
+    console.log(dir, "Starting Puppeteer");
+    const page = await browser.newPage();
+    try {
+        await page.setUserAgent((await browser.userAgent()).replace(/headless/gi, ""));
+        console.log(dir, "Creating post");
+        await page.goto(`https://www.reddit.com/${data.subreddit}/submit`);
+        console.log(dir, "Adding title");
+        await page.type('[placeholder="Title"]', data.title);
+        switch (data.type) {
+            case "text":
+            case "post": {
+                await page.waitForSelector('::-p-xpath(//*[.//i and text()="Post"])');
+                await page.click('::-p-xpath(//*[.//i and text()="Post"])');
+                if (data.body) {
+                    console.log(dir, "Adding body");
+                    let markdown = await page.$('::-p-xpath(//*[text()="Markdown Mode"])');
+                    if (markdown) {
+                        await markdown.click();
+                    }
+                    await page.waitForSelector('[placeholder="Text (optional)"]');
+                    await page.type('[placeholder="Text (optional)"]', data.body);
+                }
+            } break;
+            case "image": {
+                await page.waitForSelector('::-p-xpath(//*[.//i and (text()="Images" or text()="Images & Video")])');
+                await page.click('::-p-xpath(//*[.//i and (text()="Images" or text()="Images & Video")])');
+                console.log(dir, "Adding image");
+                let elementHandle = await page.$('input[type="file"]');
+                await elementHandle.uploadFile(path.join(pendingDir, dir, data.file));
+            } break;
+            case "gallery":
+            case "images": {
+                await page.waitForSelector('::-p-xpath(//*[.//i and (text()="Images" or text()="Images & Video")])');
+                await page.click('::-p-xpath(//*[.//i and (text()="Images" or text()="Images & Video")])');
+                console.log(dir, "Adding images");
+                for (let image of data.images) {
+                    let elementHandle = await page.$('input[type="file"]');
+                    await elementHandle.uploadFile(path.join(pendingDir, dir, image.file));
+                    await new Promise(resolve => setTimeout(resolve, 1000));
+                }
+                let divs = [];
+                let time = Date.now();
+                while (divs.length < data.images.length) {
+                    if (Date.now() - time > 10000) {
+                        console.error(dir, "Not enough images", data);
+                        return;
+                    }
+                    divs = await page.$$('div[draggable="true"]:has([style*="background-image"])');
+                }
+                await divs[0].click();
+                await new Promise(resolve => setTimeout(resolve, 1000));
+                for (let i = data.images.length - 1; i >= 0; --i) {
+                    await divs[i].click();
+                    await new Promise(resolve => setTimeout(resolve, 1000));
+                    if (data.images[i].caption) {
+                        await page.type('[placeholder="Add a caption..."]', data.images[i].caption);
+                    }
+                    if (data.images[i].link) {
+                        await page.type('[placeholder="Add a link..."]', data.images[i].link);
+                    }
+                }
+            } break;
+            case "video": {
+                await page.waitForSelector('::-p-xpath(//*[.//i and (text()="Images" or text()="Images & Video")])');
+                await page.click('::-p-xpath(//*[.//i and (text()="Images" or text()="Images & Video")])');
+                console.log(dir, "Adding video");
+                let elementHandle = await page.$('input[type="file"]');
+                await elementHandle.uploadFile(path.join(pendingDir, dir, data.file));
+                await page.waitForSelector('::-p-xpath(//*[text()="Choose thumbnail"])', {
+                    timeout: 5 * 60 * 1000
+                });
+                if (data.thumbnail) {
+                    console.log(dir, "Choosing thumbnail");
+                    await page.click('::-p-xpath(//*[./*[not(.//*) and text()="Choose thumbnail"]])');
+                    await page.waitForSelector('div:has(> div:nth-child(10):last-child > img)');
+                    await page.click(`div:has(> div:nth-child(10):last-child > img) > div:nth-child(${data.thumbnail})`);
+                    await page.click('::-p-xpath(//*[text()="Select"])');
+                }
+                if (data.gif) {
+                    await page.click('::-p-xpath(//*[text()="Make GIF"])');
+                }
+            } break;
+            case "url":
+            case "link": {
+                await page.waitForSelector('::-p-xpath(//*[.//i and text()="Link"])');
+                await page.click('::-p-xpath(//*[.//i and text()="Link"])');
+                console.log(dir, "Adding url");
+                await page.waitForSelector('[placeholder="Url"]');
+                await page.type('[placeholder="Url"]', data.url);
+            } break;
+        }
+        console.log(dir, "Setting tags and flair");
+        if (data.oc) {
+            await page.click('::-p-xpath(//*[text()="OC"])');
+        }
+        if (data.spoiler) {
+            await page.click('::-p-xpath(//*[text()="Spoiler"])');
+        }
+        if (data.nsfw) {
+            await page.click('::-p-xpath(//*[text()="NSFW"])');
+        }
+        if (data.flair) {
+            await page.click('::-p-xpath(//*[text()="Flair"])');
+            await page.click(`[aria-label="flair_picker"] ::-p-xpath(//*[text()=${JSON.stringify(data.flair)}])`);
+            await page.click('::-p-xpath(//*[text()="Apply"])');
+        }
+        await page.waitForSelector('::-p-xpath(//*[not(.//i) and text()="Post" and not(@disabled)])');
+        await page.click('::-p-xpath(//*[not(.//i) and text()="Post" and not(@disabled)])');
+        await page.waitForNavigation();
+        if (data.comments) {
+            console.log(dir, "Adding comments");
+            let markdown = await page.$('::-p-xpath(//*[text()="Markdown Mode"])');
+            if (markdown) {
+                await markdown.click();
+            }
+            for (let comment of data.comments) {
+                await page.waitForSelector('[placeholder="What are your thoughts?"]');
+                await page.type('[placeholder="What are your thoughts?"]', comment);
+                await page.click('[type="submit"]');
+                await page.waitForNetworkIdle();
+            }
+        }
+        await fs.rename(path.join(pendingDir, dir), path.join(doneDir, dir));
+        console.log(dir, "Posted", page.url(), data);
+    } catch (e) {
+        console.error(dir, "Error", data, e);
+    } finally {
+        await page.close();
+    }
+}
+
+function schedule(browser, dir) {
+    try {
+        let time = Date.parse(dir.replace(/^(\d+)-(\d+)-(\d+) (\d+)-(\d+)-(\d+)$/,
+            "$1-$2-$3T$4:$5:$6"));
+        if (!scheduled.hasOwnProperty(time)) {
+            scheduled[time] = setTimeout(post, time - Date.now(), browser, dir);
+            console.log(dir, "Scheduled");
+        }
+    } catch (e) {
+        console.error(e);
+    }
+}
+
+async function scheduleAll(browser) {
+    for (let dir of await fs.readdir(pendingDir)) {
+        schedule(browser, dir);
+    }
+}
+
+(async () => {
+    console.log("Starting", Date());
+    await fs.mkdir(userDataDir, {recursive: true}).catch(() => {});
+    await fs.mkdir(pendingDir, {recursive: true}).catch(() => {});
+    await fs.mkdir(doneDir, {recursive: true}).catch(() => {});
+    const browser = await puppeteer.launch({
+        headless: "new",
+        userDataDir
+    });
+    const page = await browser.newPage();
+    await page.setUserAgent((await browser.userAgent()).replace(/headless/gi, ""));
+    console.log("Signing in");
+    await page.goto("https://www.reddit.com/login/");
+    try {
+        await page.type('#loginUsername', process.env.USERNAME);
+        await page.type('#loginPassword', process.env.PASSWORD);
+        await page.click('.AnimatedForm [type="submit"]');
+        await page.waitForNetworkIdle();
+        console.log("Signed in");
+    } catch (e) {
+        console.log("Skipping sign in");
+    }
+    await page.close();
+    await scheduleAll(browser);
+    for await (let event of fs.watch(pendingDir)) {
+        const filepath = path.join(pendingDir, event.filename);
+        switch (event.filename) {
+            case "refresh":
+                if (await new Promise(resolve => fs.access(filepath)
+                    .then(() => resolve(true), () => resolve(false)))) {
+                    await fs.rm(filepath).catch(() => fs.rmdir(filepath));
+                    await scheduleAll(browser);
+                    break;
+                }
+                // falls through
+            default:
+                schedule(browser, event.filename);
+        }
+    }
+})();
