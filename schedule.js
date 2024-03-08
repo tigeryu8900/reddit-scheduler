71d854bcde67a53341b6e9eb5361f428c4086646
tigeryu8900 <tigeryu8900@gmail.com>
2024-03-08 05:33:19 -0800

    transfer files one by one

--- schedule.js
+++ schedule.js
@@ -8,20 +8,24 @@ import * as crypto from "crypto";
 const pendingDir = path.join(os.homedir(), ".reddit", "pending");
 const userDataDir = path.join(os.homedir(), ".reddit", "Scheduler User Data");
 
-async function saveDataBase(dir, data, files) {
+async function getTmpdir() {
   const tmpdir = path.join(os.tmpdir(), crypto.randomUUID());
   await fs.mkdir(tmpdir, {recursive: true});
-  for (const name in files) {
-    const file = files[name];
-    switch (file.type) {
-      case "path":
-        await fs.symlink(file.data.replaceAll("/", path.sep), path.join(tmpdir, name));
-        break;
-      case "binary":
-        await fs.writeFile(path.join(tmpdir, name), Buffer.from(file.data, "binary"));
-        break;
-    }
+  return tmpdir;
+}
+
+async function addFile(tmpdir, file, name) {
+  switch (file.type) {
+    case "path":
+      await fs.symlink(file.data.replaceAll("/", path.sep), path.join(tmpdir, name));
+      break;
+    case "binary":
+      await fs.writeFile(path.join(tmpdir, name), Buffer.from(file.data, "binary"));
+      break;
   }
+}
+
+async function finishSaveData(tmpdir, dir, data) {
   await fs.writeFile(path.join(tmpdir, "data.json"), JSON.stringify(data, null, 2));
   await fs.rename(tmpdir, path.join(pendingDir, dir));
 }
@@ -38,13 +42,16 @@ async function saveDataBase(dir, data, files) {
   });
   const page = await browser.newPage();
   await page.goto("file://" + path.resolve("schedule.html").replaceAll(path.delimiter, "/"));
-  await page.exposeFunction("saveDataBase", saveDataBase);
+  await page.exposeFunction("getTmpdir", getTmpdir);
+  await page.exposeFunction("addFile", addFile);
+  await page.exposeFunction("finishSaveData", finishSaveData);
   await page.evaluate(async () => await new Promise(resolve => {
-    window.saveData = function (dir, data, files) {
-      saveDataBase(dir, data, files).then(() => {
-        alert(`${dir} saved`);
-        resolve();
-      });
+    window.saveData = async function (dir, data, files) {
+      const tmpdir = await getTmpdir();
+      await Promise.all(Object.entries(files).map(([name, file]) => addFile(tmpdir, file, name)));
+      await finishSaveData(tmpdir, dir, data);
+      alert(`${dir} saved`);
+      resolve();
     }
   }));
   await browser.close();
